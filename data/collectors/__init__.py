fe06aae69bc59753a7c4b124f22f4bae5b35c3e2
TheEdgeEquation <jrodjis@live.com>
2026-04-11 18:52:44 -0500

    Add data/collectors/__init__.py

---
+++ data/collectors/__init__.py
@@ -0,0 +1,21 @@
+from .mlb_collector import MLBCollector
+from .nhl_collector import NHLCollector
+from .nba_collector import NBACollector
+from .nfl_collector import NFLCollector
+from .kbo_collector import KBOCollector
+from .npb_collector import NPBCollector
+from .afl_collector import AFLCollector
+from .ipl_collector import IPLCollector
+from .euro_soccer_collector import EuroSoccerCollector
+
+__all__ = [
+    "MLBCollector",
+    "NHLCollector",
+    "NBACollector",
+    "NFLCollector",
+    "KBOCollector",
+    "NPBCollector",
+    "AFLCollector",
+    "IPLCollector",
+    "EuroSoccerCollector",
+]
