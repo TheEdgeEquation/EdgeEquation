df3160a8c6e1a673fcaf9a114acd5a8b6fe91d02
TheEdgeEquation <jrodjis@live.com>
2026-04-15 20:44:41 -0500

    Add validation for global games list

--- engine/cbc_projector.py
+++ engine/cbc_projector.py
@@ -93,6 +93,10 @@ def get_ucl_projections():
     ...
     from engine.global_clv_helper import attach_clv_to_list
     return attach_clv_to_list(games)
+ from engine.global_validator import validate_global_list
+games = validate_global_list(games)
+
+return games
 
  
  
