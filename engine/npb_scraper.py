3ccfbf94490222fd2e9a5434d1d0c814b8cf44c0
TheEdgeEquation <jrodjis@live.com>
2026-04-15 20:47:05 -0500

    Validate games list after scraping NPB schedule
    Added validation for the games list after scraping the NPB schedule.

--- engine/npb_scraper.py
+++ engine/npb_scraper.py
@@ -109,4 +109,8 @@ def get_npb_projections(game_date: date = None):
     logger.info("NPB: " + str(len(games)) + " games found for " + str(gd))
     from engine.global_clv_helper import attach_clv_to_list
 return attach_clv_to_list(games)
+from engine.global_validator import validate_global_list
+games = validate_global_list(games)
+
+return games
 
