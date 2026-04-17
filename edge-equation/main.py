e3f38518c077af5247da72bc0714c6a338b5533c
TheEdgeEquation <jrodjis@live.com>
2026-04-16 20:43:44 -0500

    Update main.py

--- edge-equation/main.py
+++ edge-equation/main.py
@@ -2,7 +2,9 @@
 import argparse
 
 from engines.facts import post_domestic_fact, post_overseas_fact
-# we'll add more imports later as we build other engines
+from engines.spotlight import post_spotlight_insight
+from engines.edges import post_morning_edges, post_evening_edges
+from engines.results import post_results_if_any
 
 
 def main():
@@ -12,8 +14,22 @@ def main():
 
     if args.mode == "fact_domestic":
         post_domestic_fact()
+
     elif args.mode == "fact_overseas":
         post_overseas_fact()
+
+    elif args.mode == "spotlight":
+        post_spotlight_insight()
+
+    elif args.mode == "edges_morning":
+        post_morning_edges()
+
+    elif args.mode == "edges_evening":
+        post_evening_edges()
+
+    elif args.mode == "results":
+        post_results_if_any()
+
     else:
         raise SystemExit(f"Unknown mode: {args.mode}")
 
