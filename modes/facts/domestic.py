03af497012c637dd3f5d7311a7b2d9b4979bca90
TheEdgeEquation <jrodjis@live.com>
2026-04-17 11:51:21 -0500

    Change posting function from post_to_x to post_text

--- modes/facts/domestic.py
+++ modes/facts/domestic.py
@@ -1,7 +1,7 @@
-from core.posting import post_to_x
+from core.posting import post_text
 from core.formatting import format_domestic_fact
 
 def run():
     text = format_domestic_fact()
-    post_to_x(text)
+    post_text(text)
     return text
