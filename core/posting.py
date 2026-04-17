a1fb47a06b103447d43491141ffce546ac773aa7
TheEdgeEquation <jrodjis@live.com>
2026-04-17 11:49:43 -0500

    Update posting.py

--- core/posting.py
+++ core/posting.py
@@ -1,7 +1,14 @@
 # core/posting.py
 
 import time
-from core.x_client import get_x_client
+from core.posting import post_text
+from core.formatting import format_domestic_fact
+
+def run():
+    text = format_domestic_fact()
+    post_text(text)
+    return text
+
 
 def _log(message: str):
     """Simple internal logger."""
